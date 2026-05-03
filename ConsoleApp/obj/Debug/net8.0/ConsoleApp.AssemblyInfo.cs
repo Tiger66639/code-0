b6a412a418ed5c4612e615c84603d5c85217fe53
Alin Smith <Tiger66639@live.de>
2026-05-03 20:24:39 +0000

    Update assembly information and project files for .NET 8.0 compatibility

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8cb3da29a37ec8f80b7258decd098de96fd8ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46619bdca2ad9ae93080d22af4faf5e23824ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
